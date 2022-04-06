4a57f974acd00849152e63ae8a8ee40d21e3e886
Alex Space <cometospace689@gmail.com>
2022-04-06 17:29:17 +0600

    Added ShopPage, collection-preview and collection-item components

--- src/App.js
+++ src/App.js
@@ -1,27 +1,13 @@
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component';
 import { Routes, Route } from 'react-router-dom';
-
-const TopicsList = () => {
-  return (
-  <div>
-    TopicsList page
-  </div>
-  );
-};
-
-const TopicDetail = () => (
-  <div>
-    TopicDetail page
-  </div>
-);
 
 function App() {
   return (
     <Routes>
-      <Route exec={false} path='/' element={<HomePage />} />
-      <Route path='/topics' element={<TopicsList />} />
-      <Route path='/topics/:tipicId' element={<TopicDetail />} />
+      <Route exect path='/' element={<HomePage />} />
+      <Route path='/shop' element={<ShopPage />} />
     </Routes>
   );
 }
