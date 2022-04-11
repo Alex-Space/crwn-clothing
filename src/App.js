826a47cc1dc1e7d996a928700535d75c37b63566
Alex Space <cometospace689@gmail.com>
2022-04-11 23:42:23 +0600

    Implemented firebase utils, included ability to stro authenticated user into our firestrore data base

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import SignInAndSignUpPage from './pages/sing-in-and-sign-up/sing-in-and-sign-up
 import { Routes, Route } from 'react-router-dom';
 import Header from './components/header/header.component';
 import React from 'react';
-import {auth} from './firebase/firebase.utils';
+import {auth, createUserProfileDocument} from './firebase/firebase.utils';
 
 class App extends React.Component {
   constructor() {
@@ -19,10 +19,23 @@ class App extends React.Component {
   unsubscribeFromAuth = null;
 
   componentDidMount() {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
-      this.setState({ currentUser: user });
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+      
+      if (userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
 
-      console.log(user);
+        userRef.onSnapshot(snapShot => {
+          this.setState({
+            currentUser: {
+              id: snapShot.id,
+              ...snapShot.data()
+            }
+          });
+          console.log(this.state);
+        });
+      }
+
+      this.setState({currentUser: userAuth});
     });
   }
 
